3b920e7dd3cf8d2add67a7119fcb475c461b4940
Vishnu tyagi <vishnu.tyagi.mec20@itbhu.ac.in>
2024-07-29 14:24:14 +0530

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,44 +1,48 @@
-import React from 'react'
-import ReactMemo from './ReactMemo'
-import UseMemo from './UseMemo'
-import { useState,useMemo } from 'react'
+import React, { useState, useMemo } from 'react';
+import ReactMemo from './ReactMemo';
+import UseMemo from './UseMemo';
 
 function App() {
-    const [todo,setTodo]=useState([]);
-    const [task,setTask]=useState([]);
-    const [count,setCount]=useState(0);
-    const [todoname,setTodoname]=useState("New Todo");
-    // const calculation = useMemo(() => clickedcount(), [count]);
+  const [todo, setTodo] = useState([]);
+  const [count, setCount] = useState(0);
+  const [todoname, setTodoname] = useState("New Todo");
 
-    const clickedtodo=(e)=>{
-        setTodo([...todo,todoname]);
-        e.preventDefault();
-    }
-    const clickedcount=(e)=>{
-        setCount(count+1);
-        e.preventDefault();
-    }
-    return (
-        <div id="main">
-            <useMemo/>
-            <h2>My todos</h2>
-            <div>
-                {
-                    todo.map((val)=>{
-                        return <p id='todo-0'>{val}</p>;
-                    })
-                }
-            </div>
-            <button id='add-todo-btn' onClick={clickedtodo}>Add Todo</button>
-            <hr></hr>
-            <p id='incr-cnt'>Count: <button id='incr-btn' onClick={clickedcount}>{count}</button></p>
-            <h2 id='item-jumbotron'>jumbotron</h2>
-            <p id='calc'>1000000000</p>
-            <hr></hr>
-            <hr></hr>
-            <ReactMemo/>
-        </div>
-    )
+  const clickedtodo = (e) => {
+    setTodo([...todo, todoname]);
+    e.preventDefault();
+  };
+
+  const clickedcount = (e) => {
+    setCount(count + 1);
+    e.preventDefault();
+  };
+
+  // Example useMemo usage: 
+  // You can use useMemo to memoize a calculation or a derived state.
+  const expensiveCalculation = useMemo(() => {
+    console.log("Calculating...");
+    return count + 1000000000;
+  }, [count]);
+
+  return (
+    <div id="main">
+      <UseMemo />
+      <h2>My Todos</h2>
+      <div>
+        {todo.map((val, index) => (
+          <p key={index} id={`todo-${index}`}>{val}</p>
+        ))}
+      </div>
+      <button id='add-todo-btn' onClick={clickedtodo}>Add Todo</button>
+      <hr />
+      <p id='incr-cnt'>Count: {count}</p>
+      <button id='incr-btn' onClick={clickedcount}>Increment Count</button>
+      <h2 id='item-jumbotron'>Jumbotron</h2>
+      <p id='calc'>{expensiveCalculation}</p>
+      <hr />
+      <ReactMemo />
+    </div>
+  );
 }
 
-export default App
+export default App;
